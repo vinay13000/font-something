f9a7352bfa2c1c65eca5d56b3133140de8fd8458
vinay13000 <74045128+vinay13000@users.noreply.github.com>
2021-10-20 19:23:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-2020
+function setup(){
+    video=createCapture(VIDEO);
+    video.size(550,550);
+    canvas=createCanvas(550,550);
+    canvas.position(560,150);
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotPoses)
+}
+function draw(){
+    background('#2193b0');
+  
+}
+
+function modelLoaded(){
+    console.log("posenet started the job")
+
+}
+
+function gotPoses(results){
+
+    if(results.length>0){
+        console.log(results);
+
+    }
+}
